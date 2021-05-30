565657ebf69e9953b943d07aa44364957b602d7f
Timothy Kipkorir Serem <kipkorirserem3@gmail.com>
2021-05-30 20:58:27 +0300

    Creating pickers

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -9,19 +9,43 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
+    
     @State private var checkAmount = ""
     @State private var numberOfPeople = 2
     @State private var tipPercentage = 2
     let tipPercentages = [10, 15, 20, 25, 0]
+    
     var body: some View {
-        Form {
-            Section {
-                TextField("Amount", text: $checkAmount)
-                    .keyboardType(.decimalPad)
+        NavigationView {
+            Form {
+                
+                Section {
+                    TextField("Amount", text: $checkAmount)
+                        .keyboardType(.decimalPad)
+                    
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2 ..< 20) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                
+                Section(header: Text("How much tip do you want to leave?")) {
+                    Picker("Tip Percentage", selection: $tipPercentage) {
+                        ForEach(0 ..< tipPercentages.count) {
+                            Text("\(self.tipPercentages[$0])%")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Section {
+                    Text("\(checkAmount)")
+                }
+                
             }
-            Section {
-                Text("\(checkAmount)")
-            }
+            
+            .navigationTitle("WeSplit")
         }
     }
 }
