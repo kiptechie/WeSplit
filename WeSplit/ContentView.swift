a9715ae6eb437309352b99421cee167a64fa8b78
Timothy Kipkorir Serem <kipkorirserem3@gmail.com>
2021-05-30 21:26:57 +0300

    Using stacks to arrange views

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -9,55 +9,38 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
-    
-    @State private var checkAmount = ""
-    @State private var numberOfPeople = 2
-    @State private var tipPercentage = 2
-    let tipPercentages = [10, 15, 20, 25, 0]
-    
-    var totalPerPerson: Double {
-        // calculate total per person here
-        let peopleCount = Double(numberOfPeople + 2)
-        let tipSelection = Double(tipPercentages[tipPercentage])
-        let orderAmount = Double(checkAmount) ?? 0
-        
-        let tipValue = orderAmount / 100 * tipSelection
-        let grandTotal = orderAmount + tipValue
-        let amountPerPerson = grandTotal / peopleCount
-        return amountPerPerson
-    }
-    
     var body: some View {
-        NavigationView {
-            Form {
-                
-                Section {
-                    TextField("Amount", text: $checkAmount)
-                        .keyboardType(.decimalPad)
-                    
-                    Picker("Number of people", selection: $numberOfPeople) {
-                        ForEach(2 ..< 20) {
-                            Text("\($0) people")
-                        }
-                    }
+        Form {
+            Section {
+                VStack(
+                    alignment: .leading,
+                    spacing: 20
+                ) {
+                    Text("Hello iOS")
+                    Text("This is another Vstack text view")
                 }
-                
-                Section(header: Text("How much tip do you want to leave?")) {
-                    Picker("Tip Percentage", selection: $tipPercentage) {
-                        ForEach(0 ..< tipPercentages.count) {
-                            Text("\(self.tipPercentages[$0])%")
-                        }
-                    }
-                    .pickerStyle(SegmentedPickerStyle())
-                }
-                
-                Section {
-                    Text("KES \(totalPerPerson, specifier: "%.2f")")
-                }
-                
             }
-            
-            .navigationTitle("WeSplit")
+            Section {
+                VStack {
+                    Text("Hello iOS")
+                    Text("This is another Vstack text view with Spacer()")
+                    Spacer()
+                }
+            }
+            Section {
+                HStack(
+                    spacing: 20
+                ) {
+                    Text("Hello iOS")
+                    Text("This is another Hstack text view")
+                }
+            }
+            Section {
+                ZStack {
+                    Text("Hello iOS")
+                    Text("This is another Zstack text view")
+                }
+            }
         }
     }
 }
