3902c734d303eb466c8d5ee3d3115a46e452f87f
Timothy Kipkorir Serem <kipkorirserem3@gmail.com>
2021-05-30 21:47:50 +0300

    Buttons with images

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -10,11 +10,19 @@ import CoreData
 
 struct ContentView: View {
     var body: some View {
-        //LinearGradient(gradient: /*@START_MENU_TOKEN@*/Gradient(colors: [Color.red, Color.blue])/*@END_MENU_TOKEN@*/, startPoint: /*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/, endPoint: /*@START_MENU_TOKEN@*/.trailing/*@END_MENU_TOKEN@*/)
-        
-        //RadialGradient(gradient: /*@START_MENU_TOKEN@*/Gradient(colors: [Color.red, Color.blue])/*@END_MENU_TOKEN@*/, center: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, startRadius: /*@START_MENU_TOKEN@*/5/*@END_MENU_TOKEN@*/, endRadius: /*@START_MENU_TOKEN@*/500/*@END_MENU_TOKEN@*/)
-        
-        AngularGradient(gradient: Gradient(colors: [Color.red, Color.blue, Color.green, Color.gray, Color.purple]), center: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+        Form {
+            Button("Tap me!") {
+                print("Button was tapped")
+            }
+            Button(action: {
+                print("Button 2 tapped")
+            }) {
+                HStack(spacing: 10) {
+                    Image(systemName: "pencil")
+                    Text("Edit")
+                }
+            }
+        }
     }
 }
 
